abc2fa226655a45f7553c7a1ab47826a80f943f1
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-13 01:41:39 -0500

    Creating new gql query

--- services/index.js
+++ services/index.js
@@ -37,3 +37,29 @@ export const getPosts = async () => {
 
     return result.postsConnection.edges;
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+      ) {
+        title
+        coverImage {
+          coverImagePost {
+            coverImage {
+              url
+            }
+          }
+        }
+        createdAt
+        slug
+      }
+    }
+  `
+
+  const result = await request(graphqlAPI, query);
+
+  return result.posts;
+}
