ad731fd67fea58216c3b676992827bb67520b769
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-13 03:10:40 -0500

    Working on PostWidget component

--- components/PostWidget.jsx
+++ components/PostWidget.jsx
@@ -2,24 +2,42 @@ import React, { useEffect, useState } from 'react'
 import moment from 'moment'
 import Link from 'next/link'
 
-import { getRecentPosts } from '../services'
+import { getRecentPosts, getSimilarPosts } from '../services'
 
 const PostWidget = ({ categories, slug }) => {
     const [relatedPosts, setRelatedPosts] = useState([])
 
     useEffect(() => {
         if(slug) {
-            getSimilarPosts(category, slug)
+            getSimilarPosts(categories, slug)
                 .then((res) => setRelatedPosts(res))
         } else {
             getRecentPosts()
                 .then((res) => setRelatedPosts(res))
         }
-    }, [])
+    }, [slug])
 
     return (
-        <div>
-            
+        <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+            <h3 className="text-xl mb-8 font-semibold border-b pb-4">
+                {slug ? 'Related Posts' : 'Recent Posts'}
+                {relatedPosts.map((post) => (
+                    <div key={post.title} className="flex item-center w-full mb-4">
+                        <div className="w-16 flex-none">
+                            <img 
+                                alt={post.title}
+                                height="60px"
+                                width="60px"
+                                className="align-middle rounded-full"
+                                src={post.coverImage.coverImagePost[0].coverImage.url}
+                            />
+                        </div>
+                        <div className="flex-grow ml-4">
+
+                        </div>
+                    </div>
+                ))}
+            </h3>
         </div>
     )
 }
