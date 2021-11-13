f76eb300db83dc66369ee8232d8f892478311a72
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-11-13 01:20:39 -0500

    Creating PostCard layout

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -13,6 +13,23 @@ const PostCard = ({ post }) => {
                     className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
                 />
             </div>
+            <h1 className="transition duration-700 text-center mb-8 cursor-pointer hover:text-pink-600 text-3xl font-semibold">
+                <Link href={`/post/${post.slug}`}>
+                    {post.title}
+                </Link>
+            </h1>
+            <div className="block lg:flex text-center items-center justify-center mb-8 w-full">
+                <div className="flex items-center justify-center mb-4 lg:mb-0 w-full lg:w-auto mr-9">
+                    <img 
+                        alt={post.author.name}
+                        height="30px"
+                        width="30px"
+                        className="align-middle rounded-full"
+                        src={post.author.picture.url}
+                    />
+                    <p className="inline align-middle text-gray-700 ml-2 text-lg">{post.author.name}</p>
+                </div>
+            </div>
         </div>
     )
 }
